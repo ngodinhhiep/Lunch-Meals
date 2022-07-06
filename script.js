37afc105e32f06f7c662b5626f02ede5a3a36a00
Ngo Dinh Hiep <95422021+ngodinhhiep@users.noreply.github.com>
2022-07-06 14:52:53 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+document.querySelector('.menu').addEventListener('click', () => {
+    document.querySelectorAll('.target').forEach((item) => {
+        item.classList.toggle('change')
+    })
+})
 const icons = document.querySelectorAll('.section-1-icons i');
 let i = 1;
 
@@ -17,3 +22,4 @@ setInterval(() => {
         icon.nextElementSibling.classList.add('change');
     }
 },4000)
+
